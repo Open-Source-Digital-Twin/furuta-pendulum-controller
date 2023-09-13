63e128f59df8524f9262e9ef45dc227030f6d1b1
LLagu <lagunasluca@protonmail.com>
2023-09-13 14:48:14 +0000

    initial tests

---
+++ src/hal/AngleSensorProto.h
@@ -0,0 +1,11 @@
+#ifndef ANGLE_SENSOR_PROTO_H
+#define ANGLE_SENSOR_PROTO_H
+
+#include "SensorInterface.h"
+// #include "sensors.pb.h"
+
+class AngleSensorProto : public SensorInterface {
+
+};
+
+#endif
---
+++ src/hal/DCMotorProto.h
@@ -0,0 +1,10 @@
+#ifndef DC_MOTOR_PROTO_H
+#define DC_MOTOR_PROTO_H
+
+#include "ActuatorInterface.h"
+
+class DCMotorProto : public ActuatorInterface{
+
+};
+
+#endif
