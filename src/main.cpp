0486a62b5c7c50b41eab1e22db9fe4777c3ad920
Caio Piccirillo <caiopiccirillo@gmail.com>
2023-10-23 20:12:03 +0200

    Add spdlog and abseil as dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,21 @@
 #include "Configuration.h"
 #include "Controller.h"
 #include "DCMotor.h"
+#include "absl/strings/str_join.h"
+#include "spdlog/spdlog.h"
 #include <iostream>
 #include <memory>
+#include <vector>
 
 int main(int /*unused*/, char** /*unused*/)
 {
+    std::vector<std::string> v = { "foo", "bar", "baz" };
+    std::string s = absl::StrJoin(v, "-");
+
+    spdlog::info("Starting controller.");
+
+    spdlog::warn("Checking if abseil is working: {}", s);
+
     auto dc_motor = std::make_unique<hal::DCMotor>(0.0);
 
     [[gnu::unused]] auto angle_sensor = hal::AngleSensor(0.0);
