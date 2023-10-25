047a47dfd7b6855573907277f806a9b6f53b9663
LLagu <lagunasluca@protonmail.com>
2023-10-25 08:02:00 +0000

    wip config

--- src/config/Configuration.cpp
+++ src/config/Configuration.cpp
@@ -1,15 +1,87 @@
+// #include "Configuration.h"
+
+// namespace config {
+
+// Configuration::Configuration(std::filesystem::path filepath)
+//     : filepath_(std::move(filepath))
+// {
+// }
+
+// json Configuration::ReadConfigurationFile() { }
+
+// int Configuration::GetConfiguration(ConfigurationNameEnum ConfigName) { return 0; };
+
+// bool Configuration::WriteConfigurationFile(json jsonfile) { return true;}
+// }
+
 #include "Configuration.h"
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
 
 namespace config {
 
-Configuration::Configuration(std::filesystem::path filepath)
+Configuration::Configuration(std::filesystem::path filepath, ConfigurationNameEnum ConfigName)
     : filepath_(std::move(filepath))
 {
+    if (!std::filesystem::exists(filepath_)) {
+        CreateDefaultConfigurationFile(ConfigName);
+    }
 }
 
-json Configuration::ReadConfigurationFile() { }
-
-int Configuration::GetConfiguration(ConfigurationNameEnum ConfigName) { return 0; };
-
-bool Configuration::WriteConfigurationFile(json jsonfile) { return true;}
+json Configuration::ReadConfigurationFile()
+{
+    std::ifstream configFile(filepath_);
+    json configurationData;
+    configFile >> configurationData;
+    return configurationData;
 }
+
+int Configuration::GetConfiguration(ConfigurationNameEnum ConfigName)
+{
+    json configurationData = ReadConfigurationFile();
+    // Implement your logic to get the specific configuration value
+    return configurationData[ConfigName];
+}
+
+bool Configuration::WriteConfigurationFile(json jsonfile)
+{
+    std::ofstream configFile(filepath_);
+    configFile << std::setw(4) << jsonfile << std::endl;
+
+    return configFile.good();
+}
+
+void Configuration::CreateDefaultConfigurationFile(ConfigurationNameEnum ConfigName)
+{
+    // Create a default configuration
+    json defaultConfig;
+    switch (ConfigName) {
+        case kAngleSensorConfiguration:
+            defaultConfig["Name"] = "Angle Sensor";
+            defaultConfig["MaxValue"] = 0;
+            defaultConfig["MinValue"] = 360;
+            defaultConfig["DefaultValue"] = 0;
+            defaultConfig["Description"] = "Measures and converts mechanical rotation into a scaled elecrical signal";
+            break;
+        case kDcMotorConfiguration:
+            defaultConfig["Name"] = "DC Motor";
+            defaultConfig["MaxValue"] = 100;
+            defaultConfig["MinValue"] = 0;
+            defaultConfig["DefaultValue"] = 0;
+            defaultConfig["Description"] = "Direct current motor";
+            break;
+    }
+
+
+    bool write_success = WriteConfigurationFile(defaultConfig);
+
+    if (write_success) {
+        // This is temporary. I suppose we can use spdlog here.
+        printf("writing successfull");
+    }
+    else {
+        printf("failed to write to file");
+    }
+}
+} // namespace config
--- src/config/Configuration.h
+++ src/config/Configuration.h
@@ -4,19 +4,19 @@
 #include "ConfigurationInterface.h"
 #include <filesystem>
 #include <nlohmann/json.hpp>
-#include <string>
 using json = nlohmann::json;
 
 namespace config {
 class Configuration : public ConfigurationInterface<json> {
 public:
-    explicit Configuration(std::filesystem::path filepath);
+    explicit Configuration(std::filesystem::path filepath, ConfigurationNameEnum ConfigName);
     [[nodiscard]] json ReadConfigurationFile() override;
     [[nodiscard]] int GetConfiguration(ConfigurationNameEnum ConfigName) override;
     [[nodiscard]] bool WriteConfigurationFile(json jsonfile) override;
 
 private:
     std::filesystem::path filepath_;
+    void CreateDefaultConfigurationFile(ConfigurationNameEnum ConfigName);
 };
 } // namespace config
 
