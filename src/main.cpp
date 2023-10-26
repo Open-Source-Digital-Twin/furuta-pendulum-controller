8d1ed965a7e0f89fad087738f2ac6248411f2cc3
LLagu <lagunasluca@protonmail.com>
2023-10-26 09:23:11 +0000

    pre merge with main

--- src/main.cpp
+++ src/main.cpp
@@ -12,30 +12,30 @@ int main(int /*unused*/, char** /*unused*/)
     [[gnu::unused]] auto controller = Controller();
 
 
-    config::Configuration config("/usr/local/config/InvertedPendulum/");
+    config::Configuration config("/usr/local/config/InvertedPendulum/config.json");
 
-    // Load the existing configuration data from the JSON file
-    bool loadSuccess = config.LoadConfigurationFile();
+    // // Load the existing configuration data from the JSON file
+    // bool loadSuccess = config.LoadConfigurationFile();
 
-    if (loadSuccess) {
-        std::string targetConfigName = "Angle Sensor"; 
-        json targetConfig = config.GetConfiguration(targetConfigName);
+    // if (loadSuccess) {
+    //     std::string targetConfigName = "Angle Sensor"; 
+    //     json targetConfig = config.GetConfiguration(targetConfigName);
 
-        if (!targetConfig.empty()) {
-            // Print the configuration element
-            std::cout << "Configuration Name: " << targetConfig["Name"] << std::endl;
-            std::cout << "MaxValue: " << targetConfig["MaxValue"] << std::endl;
-            std::cout << "MinValue: " << targetConfig["MinValue"] << std::endl;
-            std::cout << "DefaultValue: " << targetConfig["DefaultValue"] << std::endl;
-            std::cout << "Description: " << targetConfig["Description"] << std::endl;
-        }
-        else {
-            std::cout << "Configuration not found." << std::endl;
-        }
-    }
-    else {
-        std::cout << "Failed to load the configuration file." << std::endl;
-    }
+    //     if (!targetConfig.empty()) {
+    //         // Print the configuration element
+    //         std::cout << "Configuration Name: " << targetConfig["Name"] << std::endl;
+    //         std::cout << "MaxValue: " << targetConfig["MaxValue"] << std::endl;
+    //         std::cout << "MinValue: " << targetConfig["MinValue"] << std::endl;
+    //         std::cout << "DefaultValue: " << targetConfig["DefaultValue"] << std::endl;
+    //         std::cout << "Description: " << targetConfig["Description"] << std::endl;
+    //     }
+    //     else {
+    //         std::cout << "Configuration not found." << std::endl;
+    //     }
+    // }
+    // else {
+    //     std::cout << "Failed to load the configuration file." << std::endl;
+    // }
 
     return 0;
 }
