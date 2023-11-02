af8a1ec6360621498a0c8ca96604910e8b47b399
Caio Piccirillo <caiopiccirillo@gmail.com>
2023-11-02 14:07:10 +0100

    Fix library linking

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "Controller.h"
 #include "DCMotor.h"
 #include "absl/strings/str_join.h"
-// #include "spdlog/spdlog.h"
+#include "spdlog/spdlog.h"
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -13,9 +13,9 @@ int main(int /*unused*/, char** /*unused*/)
     std::vector<std::string> v = { "foo", "bar", "baz" };
     std::string s = absl::StrJoin(v, "-");
 
-    // spdlog::info("Starting controller.");
+    spdlog::info("Starting controller.");
 
-    // spdlog::warn("Checking if abseil is working: {}", s);
+    spdlog::warn("Checking if abseil is working: {}", s);
 
     auto dc_motor = std::make_unique<hal::DCMotor>(0.0);
     [[gnu::unused]] auto angle_sensor = hal::AngleSensor(0.0);
