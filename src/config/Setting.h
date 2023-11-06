79d9a2bb3089d169364524e6eb017d1dafb245b1
LLagu <lagunasluca@protonmail.com>
2023-11-06 08:28:12 +0000

    Formatting Setting.h

--- src/config/Setting.h
+++ src/config/Setting.h
@@ -10,9 +10,9 @@ namespace config {
 using SettingValueType = std::variant<int, float>;
 
 /**
- * @brief Struct representing the configuration data.  
- * @remark Independent from the configuration file. Future development includes a configuration class that 
- parses the configuration file during compile time. Having a fixed size will be beneficial 
+ * @brief Struct representing the configuration data.
+ * @remark Independent from the configuration file. Future development includes a configuration class that
+ parses the configuration file during compile time. Having a fixed size will be beneficial
  when flashing resource-limited microcontrollers
  */
 struct Setting {
@@ -25,9 +25,9 @@ struct Setting {
 
 public:
     /**
-     * @brief Check if the variant holds a value of type T. 
-     * 
-     * @tparam 
+     * @brief Check if the variant holds a value of type T.
+     *
+     * @tparam
      * @return The value of type T if it holds. If not, throw an exception
      */
     template <typename T>
