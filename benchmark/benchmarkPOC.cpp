9c13030180576bcfb252a6357c8567937bc1b899
LLagu <lagunasluca@protonmail.com>
2023-11-06 12:32:10 +0000

    clang format

--- benchmark/benchmarkPOC.cpp
+++ benchmark/benchmarkPOC.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
+static void BM_StringCreation(benchmark::State& state)
+{
     for (auto _ : state) {
         std::string empty_string;
         benchmark::DoNotOptimize(empty_string);
