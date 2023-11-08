f786cc551fbff4f119d69b5454071d96942c4959
Caio Piccirillo <caiopiccirillo@gmail.com>
2023-11-08 16:44:49 +0100

    Add PID controller

---
+++ src/controller/ControllerInterface.h
@@ -0,0 +1,16 @@
+#ifndef CONTROLLER_INTERFACE_H
+#define CONTROLLER_INTERFACE_H
+
+namespace controller{
+
+template<typename LogicalInput, typename LogicalOutput>
+class ControllerInterface {
+public:
+    virtual ~ControllerInterface() = default;
+    virtual void Read(LogicalInput input) = 0;
+    [[nodiscard]] virtual LogicalOutput Write() = 0;
+};
+
+}
+
+#endif
